8039e818de5801752722ce08735c2608ca178681
Caterina <ccnn1010@gmail.com>
2020-05-10 23:55:43 +1000

    changes to styles

---
+++ src/App.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import './App.css';
+import Questions from './components/questions';
+
+function App() {
+  return (
+    <div className="App">
+      <Questions />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+      super();
+ */
+     /*  this.state ={ */
+        /* loading: true, */
+     /*    trivia: []   */
+            /* {
+              id: '1',
+              question: "What alcoholic drink is made from molasses?",
+              correct_answer: "Rum",
+              incorrect_answers: [
+              "Gin",
+              "Vodka",
+              "Whisky"]
+
+            },
+                  id: '2',
+                  question: "How many colors are there in a rainbow?",
+                  correct_answer: "7",
+                  incorrect_answers: [
+                  "8",
+                  "9",
+                  "10"
+                  ]
+                 }, 
+                 
+              {
+                id: '3',
+                question: "Who is depicted on the US hundred dollar bill?",
+                correct_answer: "Benjamin Franklin",
+                incorrect_answers: [
+                "George Washington",
+                "Abraham Lincoln",
+                "Thomas Jefferson"
+                ]
+              }
+
+
+        ]
+      
+      }  */
+   /*    };
+  }
+ */
+
+  /* getQuestions = () => {
+    fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+      .then(resp => resp.json())
+      .then(resp => {
+        this.setState({ questions: resp.results }, function() {
+         this.props.history.push("/questions/1"); 
+        });
+      });
+  };
+
+  getUserAnswer = (e, id) => {
+    let questions = [...this.state.questions];
+    questions[id].user_answer = e.target.value;
+    this.setState({ questions });
+  };
+ */
+
+
+
+
+
+
+/* CompountDidMount is used because of our class component,  We are fetch from URL, taking a response and converting to JSON format so that JAvascript can understand use, take the users and set monsters to that array of users returns a promise */
+ /* componentDidMount() {
+  fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
+  .then(response => response.json()) 
+  .then(response => this.setState({ questions: response.results }));
+}  
+
+
+
+  render(){
+
+    return (
+      <div className="App">
+      {this.state.trivia.map( trivia => (
+        <p key={trivia.id}> {trivia.question}</p>
+      )
+
+      )}
+        <h1>edquiz</h1>
+        <h2>question</h2>
+        <p>A</p>
+        <p>B</p>
+        <p>C</p>
+        <p>D</p>
+        <h2>Next</h2>
+
+      </div>
+  
+ 
+  	);
+
+  }
+}
+
+export default App;
+ */
---
+++ src/components/questions.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './questions.css';
+  
+class Questions extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            trivia: [], 
+            isLoading: false,
+            error: null,
+           /*  data: null */
+        }
+    }
+
+    //fetching results from API, 10 questions general questions medium difficulty category
+    componentDidMount() {
+        this.setState({ isLoading: true })
+        /* this.setState({ question: true }); */
+        
+
+         fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
+          .then(response => {
+              if (response.ok) {
+                  return response.json();
+              } else {
+                  throw Error("Error fetching trivia!")
+              }
+          })
+          .then(trivia => {
+
+            this.setState({ trivia, isloading: false });
+     })
+        .catch(error => this.setState({ error
+        }))
+}
+    
+
+    render() { 
+
+         const { error, /* isLoading,  */ trivia } = this.state;
+
+         if (error) {
+            return <p>{ error.message }</p>
+        } 
+        /* if (isLoading) {
+            return <p>Loading...</p>
+          }  */
+         
+        return ( 
+            <div>
+                <h1>edquiz</h1>
+               {/*  {trivia.map(trivia =>(
+                    <>
+                    <h3>{ trivia.question }</h3>
+                    <p>{ trivia.correct_answer }</p>
+                    <p>{ trivia.incorrect_answers }</p>
+                    </>
+                ))} */}
+
+
+            </div>
+         ); 
+    }
+
+}
+export default Questions;
+
+/* 
+<div className = 'questions'>
+{props.trivia.map(trivia => (
+<Card key= {trivia.id} trivia={trivia} />
+))}
+</div>
+ */
